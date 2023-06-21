762a3bf5946c8405ff3492b570fa3a5001e4eb35
FOURWORDSALLCAPS <vova.zaycev39@mail.ru>
2023-06-21 21:00:55 +0300

    The function now correctly loads images

--- upload_photo.py
+++ upload_photo.py
@@ -1,17 +1,27 @@
 import requests
-import json
 
-from vkbottle import API
+from vkbottle import API, VKAPIError
 
 
-async def upload_photo_to_album(vk_token, group_id, file_path):
-    api = API(vk_token)
-    upload_url = await api.photos.get_upload_server(album_id=group_id, group_id=group_id)
-    with open(file_path, 'rb') as f:
-        photo_data = {'photo': (file_path, f, 'jpeg')}
-        response = requests.post(upload_url.upload_url, files=photo_data)
-        upload_result = json.loads(response.text)
-    photo_info = await api.photos.save(album_id=group_id, group_id=group_id, server=upload_result['server'],
-                                       photos_list=upload_result['photos_list'], hash=upload_result['hash'])
-    attachment = f"photo-{photo_info[0].owner_id}_{photo_info[0].id}"
-    return attachment
+async def upload_photo_to_album(token, group_id, album_id, file_path):
+    try:
+        api = API(token)
+        upload_url = await api.photos.get_upload_server(group_id=group_id, album_id=album_id)
+        response = requests.post(upload_url.upload_url, files={'photo': open(file_path, 'rb')})
+        response.raise_for_status()
+        if not response.ok:
+            return None
+        result_upload = response.json()
+        photos_list = result_upload['photos_list']
+        server = result_upload['server']
+        hash_value = result_upload['hash']
+        photo_info = await api.photos.save(album_id=album_id,
+                                           group_id=group_id,
+                                           server=server,
+                                           hash=hash_value,
+                                           photos_list=photos_list)
+        attachment = f"photo{photo_info[0].owner_id}_{photo_info[0].id}"
+        return attachment
+    except VKAPIError as e:
+        print(f"VK API error occurred: {e}")
+        return None
