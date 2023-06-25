46daf74dd87fca869b6cbcd8cf0fe6b2c7732df1
FOURWORDSALLCAPS <vova.zaycev39@mail.ru>
2023-06-26 00:12:27 +0300

    Revert "Update get_link_post.py"
    This reverts commit 3ce406fbe01c40a59ede0c717786102fbe2fd57f.

--- get_link_post.py
+++ get_link_post.py
@@ -6,7 +6,3 @@ def get_link_post_vk(group_id, post_id):
 def get_link_post_tg(group_id, post_id):
     post_url = f'https://t.me/{group_id[1:]}/{post_id}'
     return post_url
-
-def get_link_post_ок(ок_group_id, ок_post_id):
-    post_url = f'https://ok.ru/group/{ок_group_id}/topic/{ok_post_id}'
-    return post_url
