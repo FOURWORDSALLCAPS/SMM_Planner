eda3d9ac6d6c24e9f474e67b4d4c5fe1226ed371
FOURWORDSALLCAPS <vova.zaycev39@mail.ru>
2023-06-26 00:11:36 +0300

    Revert "Update ok.py"
    This reverts commit ba729e2506c62419912bb02d3fa3cc9471a786fa.

--- ok.py
+++ ok.py
@@ -1,3 +1,9 @@
+import requests
+import hashlib
+import time
+import json
+
+
 def create_sign(ok_public_key, ok_group_id, method, ok_access_token, ok_secret_key): 
     timestamp = str(time.time())
     sign_str = f"application_key={ok_public_key}ok_group_id={ok_group_id}method={method}type=GROUP_THEMEformat=jsonmethod={method}{timestamp}{ok_access_token}{ok_secret_key}"
@@ -8,6 +14,7 @@ def create_sign(ok_public_key, ok_group_id, method, ok_access_token, ok_secret_k
 def create_url(ok_public_key, method, sign, ok_access_token):
     url = f"https://api.ok.ru/fb.do?application_key={ok_public_key}&method={method}&format=json&sig={sign}&ok_access_token={ok_access_token}"
     return url
+
 
 def create_post_ok(ok_public_key, ok_group_id, ok_access_token, ok_secret_key, text_publication):
     method = 'mediatopic.post'
@@ -23,8 +30,7 @@ def create_post_ok(ok_public_key, ok_group_id, ok_access_token, ok_secret_key, t
     sign = create_sign(ok_public_key, ok_group_id, method, ok_access_token, ok_secret_key)    
     url = create_url(ok_public_key, method, sign, ok_access_token)
     
-    response = requests.post(url, data={'gid': ok_group_id, 'type': 'GROUP_THEME',
-                                        'attachment': json.dumps(attachment)})
+    response = requests.post(url, data={'gid': ok_group_id, 'type': 'GROUP_THEME', 'attachment': json.dumps(attachment)})
     
     try:
         response = requests.post(url, data={'gid': ok_group_id, 'type': 'GROUP_THEME', 'attachment': json.dumps(attachment)})
@@ -43,9 +49,8 @@ def create_post_ok(ok_public_key, ok_group_id, ok_access_token, ok_secret_key, t
     else:
         print('Failed to publish post.', response.status_code, response.text)
 
-    ok_post_id = response.json()
-
     return ok_post_id
+
 
 def delete_post_ok(ok_public_key, ok_group_id, ok_access_token, ok_secret_key, ok_post_id):
     method = 'mediatopic.deleteTopic'
