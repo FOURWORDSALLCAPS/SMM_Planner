0760cb66884136d1388ad1592906833d35fce4d3
Taralas209 <113886562+Taralas209@users.noreply.github.com>
2023-06-25 21:09:39 +0200

    Added function "delete_post_ok"

--- ok.py
+++ ok.py
@@ -50,6 +50,30 @@ def create_post_ok(ok_public_key, ok_group_id, ok_access_token, ok_secret_key, t
     else:
         print('Failed to publish post.', response.status_code, response.text)
 
-    ok_post_id = response.json()
-
     return ok_post_id
+
+
+def delete_post_ok(ok_public_key, ok_group_id, ok_access_token, ok_secret_key, ok_post_id):
+    method = 'mediatopic.deleteTopic'
+    
+    sign = create_sign(ok_public_key, ok_group_id, method, ok_access_token, ok_secret_key)
+    url = create_url(ok_public_key, method, sign, ok_access_token)
+    
+    response = requests.post(url, data={'gid': ok_group_id, 'topic_id': ok_post_id, 'access_token': ok_access_token})
+
+
+    try:
+        response.raise_for_status()
+    except requests.exceptions.HTTPError as errh:
+        print ("HTTP Error:",errh)
+    except requests.exceptions.ConnectionError as errc:
+        print ("Error Connecting:",errc)
+    except requests.exceptions.Timeout as errt:
+        print ("Timeout Error:",errt)
+    except requests.exceptions.RequestException as err:
+        print ("Something went wrong with the request:",err)
+    
+    if response.status_code == 200:
+        print('Post deleted successfully.')
+    else:
+        print('Failed to delete post.', response.status_code, response.text)
